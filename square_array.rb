43fe4434d6fe1cd4d6b16f267abbafc6e7727788
Christopher Jacobs <chrisj.3222@gmail.com>
2019-02-22 20:30:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
   squared_array = Array.new
   array.each do |array_value|
-    squared_array << array_value = array_value * array_value
+    puts {array_value = array_value * array_value}
   end
-  return squared_array
 end
 
 test_array = [1, 2, 3, 4]
