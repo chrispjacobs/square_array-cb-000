5ebbb22e31b07b38c6e43c54a98a4b741b204912
Christopher Jacobs <chrisj.3222@gmail.com>
2019-02-22 20:28:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  squared_array = Array.new
+  array.each do |array_value|
+    squared_array << array_value = array_value * array_value
+  end
+  return squared_array
 end
+
+test_array = [1, 2, 3, 4]
+square_array(test_array).inspect
