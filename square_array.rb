7833950837af20b24833bac07bd11d3ab16cbcf1
Christopher Jacobs <chrisj.3222@gmail.com>
2019-02-22 20:32:39 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
+  squared_array = Array.new
   array.each do |array_value|
-    puts array_value * array_value
+    new_value = array_value * array_value
+    squared_array << new_value
   end
 end
 
